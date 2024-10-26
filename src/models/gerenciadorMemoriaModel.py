af2023d099b45eb56509e185495383768b055597
Luis Gomes <luisfrancisco.15.brum@gmail.com>
2024-10-25 23:03:57 -0300

    add correçoes

--- src/models/gerenciadorMemoriaModel.py
+++ src/models/gerenciadorMemoriaModel.py
@@ -1,6 +1,10 @@
+from src.models.memoriaFisicaModel import MemoriaFisicaModel
+from src.models.memoriaVirtualModel import MemoriaVirtualModel
+
 class gerenciadorMemoriaModel(): 
-    def __init__(self):
-        pass
+    def __init__(self, memoria_virtual: MemoriaVirtualModel  = None, memoria_fisica: MemoriaFisicaModel = None):
+        self.memoria_virtual = memoria_virtual
+        self.memoria_fisica = memoria_fisica
     
-    def acha_endereco_fisico(self, memoria_virtual, memoria_fisica): 
+    def acha_endereco_fisico(self,): 
         pass
--- src/models/memoriaFisicaModel.py
+++ src/models/memoriaFisicaModel.py
@@ -1,4 +1,4 @@
 from abstratos.memoriaAbstrata import MemoriaAbstrata
-class MemoriaVirtualModel(MemoriaAbstrata):
+class MemoriaFisicaModel(MemoriaAbstrata):
     def __init__(self):
         super().__init__(numero_de_bits=16)
