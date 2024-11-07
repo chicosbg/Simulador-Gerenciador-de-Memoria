96d13497a5771b4f10b51d9a764395699b64fbfe
Y4ngfr <148384398+Y4ngfr@users.noreply.github.com>
2024-11-07 16:42:36 -0300

    Update memoriaVirtualModel.py

--- src/models/memoriaVirtualModel.py
+++ src/models/memoriaVirtualModel.py
@@ -2,10 +2,10 @@ from abstratos.memoriaAbstrata import MemoriaAbstrata
 import math
 
 class MemoriaVirtualModel(MemoriaAbstrata):
-    def __init__(self, tam_memoria_bytes):
+    def __init__(self, numero_de_paginas):
         super().__init__()
-        quantidades_de_paginas = math.log(tam_memoria_bytes, 2)
-        self.numero_paginas = quantidades_de_paginas
+        # quantidades_de_paginas = math.log(tam_memoria_bytes, 2)
+        self.numero_paginas = numero_de_paginas
         self.memoria = []
     
     def adiciona_lista_de_paginas(self, lista_paginas):
