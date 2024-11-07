6fef3356a8d7603b10b12bc8519b2e824522c140
Y4ngfr <148384398+Y4ngfr@users.noreply.github.com>
2024-11-07 16:41:02 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,32 @@
 from models.gerenciadorMemoriaModel import gerenciadorMemoriaModel
 from models.memoriaFisicaModel import MemoriaFisicaModel
 from models.memoriaVirtualModel import MemoriaVirtualModel
+import random
 
-memoria_fisica = MemoriaFisicaModel(tam_memoria_bytes=32)
-memoria_virtual = MemoriaVirtualModel(tam_memoria_bytes=32)
-memoria_virtual.adiciona_lista_de_paginas([["t","e","f"], ["e","f", "g"], ["t", "k", "l"]])
+# Tamanho de quadro/página: 4 bytes ou 32 bits
+
+memoria_fisica = MemoriaFisicaModel(tam_max_quadros=3)
+memoria_virtual = MemoriaVirtualModel(numero_de_paginas=6)
+memoria_virtual.adiciona_lista_de_paginas([["t","e","f","a"], ["e","f","g","a"], ["t","k","l","a"], ["t","k","l","a"], ["v","v","l","v"], ["t","t","s","r"]])
 arq = gerenciadorMemoriaModel(memoria_fisica=memoria_fisica, memoria_virtual=memoria_virtual)
+
+print("Memória Virtual: ", end="")
 memoria_virtual.printa_memoria()
-memoria_fisica.printa_memoria()
-print(arq.acessa_paginas(posicao_pagina=1))
-print(arq.acessa_paginas(posicao_pagina=2))
-print(arq.acessa_paginas(posicao_pagina=3))
-print(arq.acessa_paginas(posicao_pagina=1))
-memoria_virtual.printa_memoria()
+print("Memória Física: ", end="")
 memoria_fisica.printa_memoria()
 
+while (True):
+    endereco_inteiro = random.randint(0, memoria_virtual.numero_paginas*4 - 1)
+    endereco_binario = f'{endereco_inteiro:0{8}b}'
+    pagina = int(endereco_binario[0:6], 2) + 1      # 6 bits mais significativos
+    deslocamento = int(endereco_binario[6:8], 2)    # 2 bits menos significativos
+
+    print(f"Endereço gerado: {endereco_binario}")
+    print(f"Página: {pagina}, Deslocamento: {deslocamento}")
+    
+    arq.acessa_paginas(posicao_pagina=pagina)
+
+    print("Memória Física: ", end="")
+    memoria_fisica.printa_memoria()
+    
+    input()
